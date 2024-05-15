0e16fdaecd074f9514fe26bf1c85b6a75bb24712
Lifulifu <andycscsmaple@gmail.com>
2024-05-15 12:27:44 +0000

    image chat message fix

--- routes/chat.py
+++ routes/chat.py
@@ -49,13 +49,13 @@ async def handle(client: discord.Client, message: discord.Message, command: typi
             })
         else:
             # Only include images in the most recent message
-            max_images = 0 if is_newest_message else INCLUDE_IMAGES
+            max_images = INCLUDE_IMAGES if is_newest_message else 0
             user_message = construct_user_message(msg, max_images=max_images)
             history.append(user_message)
 
             if msg.reference is not None:
                 replied_message = await msg.channel.fetch_message(msg.reference.message_id)
-                replied_message = construct_user_message(msg, max_images=max_images)
+                replied_message = construct_user_message(replied_message, max_images=max_images)
                 history.append(replied_message)
 
         is_newest_message = False
@@ -68,10 +68,10 @@ async def handle(client: discord.Client, message: discord.Message, command: typi
         *history,
     ]
     print(input_messages)
+
     try:
         res = chat(input_messages)
         await message.channel.send(res)
     except Exception as e:
         print(e)
         await message.channel.send('發生錯誤，請聯絡 @lifu')
-
